97816955c7aa81c8a3e3b307ad38727941f0f3e3
Hans Hübner <hans.huebner@gmail.com>
2025-05-21 17:42:25 +0200

    check build status and fix line endings of listing file

--- assemble.sh
+++ assemble.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+set -e
+
 # Build the Docker image if it doesn't exist
 if ! docker image inspect rt11-assembler >/dev/null 2>&1; then
     echo "Building Docker image..."
     docker build -t rt11-assembler builder/
 fi
 
+# Fix line endings of listing file when exiting
+trap "perl -pi -e 's/\r//g' FORTH.LST" 0
+
 # Run the container with the current directory mounted to /app/data
-docker run -it --rm \
+if docker run -it --rm \
     -v "$(pwd):/app/data" \
     rt11-assembler ./build.sh
-
-echo
-echo FIG FORTH standalone binary built:
-ls -l forth.bin
+then
+  echo
+  echo FIG FORTH standalone binary built:
+  ls -l forth.bin
+else
+  echo build failed
+fi
