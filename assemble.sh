92a92d6ce294c8eabc4074ac9eb533b94a059d91
Hans Hübner <hans.huebner@gmail.com>
2025-05-22 19:26:27 +0200

    Always rebuild docker container to pic up changes

--- assemble.sh
+++ assemble.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-# Build the Docker image if it doesn't exist
-if ! docker image inspect rt11-assembler >/dev/null 2>&1; then
-    echo "Building Docker image..."
-    docker build -t rt11-assembler builder/
-fi
+docker build -t rt11-assembler builder/
 
 # Fix line endings of listing file when exiting
 trap "perl -pi -e 's/\r//g' FORTH.LST" 0
